0d72b0ae8018dd07af470104d8b157a77b03f100
Dedicker Benoit <dedickerbenoit@gmail.com>
2026-02-09 15:28:44 +0100

    feat(i18n): add i18n config and french translations

---
+++ src/locales/fr.ts
@@ -0,0 +1,16 @@
+export default {
+  auth: {
+    login: {
+      trigger: "Se connecter",
+      title: "Connexion",
+      email: "Email",
+      emailPlaceholder: "vous@exemple.com",
+      password: "Mot de passe",
+      passwordPlaceholder: "••••••••",
+      submit: "Se connecter",
+      error: "Email ou mot de passe incorrect",
+      noAccount: "Pas encore de compte ?",
+      createAccount: "Créer un compte",
+    },
+  },
+} as const;
